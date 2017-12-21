3c8d932f84021615ab5effd7a209262060f53774
Diego Gastón <dienan16@gmail.com>
2017-12-21 12:31:49 -0300

    Add project files.

---
+++ AirDemo.Specs/Search/SearchHotel.feature
@@ -0,0 +1,16 @@
+﻿@FullRegression
+Feature: SearchHotel
+	In order to search for an hotel room
+	As a pinSight user
+	I want search hotel results to be displayed on screen
+
+Background: Login
+	Given That Im logged in at pinSight
+
+@search
+Scenario: Search Hotel main path
+	And I have entered Barcelona, Spain - Barcelona  El Prat Arpt (BCN) as the city
+	And I have entered checkin date
+	And I have entered checkout date
+	When I click on Search button
+	Then Hotel search results should be displayed on screen
---
+++ AirDemo.Specs/Search/SearchHotelSteps.cs
@@ -0,0 +1,65 @@
+﻿using OpenQA.Selenium;
+using System;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
+
+namespace HOTELpinSight.Pages
+{
+    [Binding]
+    public class SearchHotelSteps: Steps
+    {
+
+        /// <summary>
+        /// Web Driver
+        /// </summary>
+        private IWebDriver _driver;        
+        private HotelSearchPage _hotelSearchPage; //Check how to instanciate should be sth like new HotelSearchPage
+        private SearchingPage _searchingPage;
+        private ResultsPage _resultsPage;
+
+        [Given(@"That Im logged in at pinSight")]
+        public void GivenThatImLoggedInAtPinSight()
+        {
+            Given("Given that I navigate to pinSight application");
+            Given("And I enter testuser@mailinator.com as the username");
+            Given("I enter the password");
+            When("I press login");
+            Then("I should land on the Hotel page");
+            //Assert.IsTrue(_driver.Title.Equals("P UAT Agency(987654) :: Home"));
+        }
+        
+        [Given(@"I have entered (.*) as the city")]
+        public void GivenIHaveEnteredCity(string search)
+        {
+            _hotelSearchPage.Search(search);          
+            
+        }
+        
+        [Given(@"I have entered checkin date")]
+        public void GivenIHaveEnteredCheckinDate(string checkIn)
+        {
+            _hotelSearchPage.SelectCheckIn();
+        }
+        
+        [Given(@"I have entered checkout date")]
+        public void GivenIHaveEnteredCheckoutDate(string checkOut)
+        {
+            //_hotelSearchPage.EnterCheckOut = checkOut;
+            
+        }
+        
+        [When(@"I click on Search button")]
+        public void WhenIClickOnSearchButton()
+        {
+            _hotelSearchPage.ClickSearchHotel();
+        }
+        
+        [Then(@"Hotel search results should be displayed on screen")]
+        public void ThenHotelSearchResultsShouldBeDisplayedOnScreen()
+        {
+            _searchingPage = new SearchingPage(_driver);
+            _searchingPage.EnsurePageIsLoaded();            
+        }
+    }
+}
